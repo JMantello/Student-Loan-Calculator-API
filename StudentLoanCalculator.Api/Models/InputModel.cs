da1b8f2eb2415da3108f13a783a413fa219fa97b
Jonathan Mantello <Jonathan.Mantello@Envestnet.com>
2022-07-05 18:36:09 -0700

    Modified input and output models, added 'yearly' output functions, reconfigured Cors, and refactored MongoCRUD, allowing for non-db functionality.

--- StudentLoanCalculator.Api/Models/InputModel.cs
+++ StudentLoanCalculator.Api/Models/InputModel.cs
@@ -7,10 +7,11 @@
         public double LoanAmount { get; set; }
         public double InterestRate { get; set; }
         public int TermInYears { get; set; } 
-        public double MinimumPayment { get; set; }
+        public double MinimumPayment { get; set; } // Take out
         
         // Investment Risk
-        public double InvestmentGrowthRate { get; set; } 
+        public double InvestmentGrowthRate { get; set; }
+        public string InvestmentRisk { get; set; }
 
     }
 }
--- StudentLoanCalculator.Api/Program.cs
+++ StudentLoanCalculator.Api/Program.cs
@@ -2,6 +2,7 @@ using StudentLoanCalculator.Domain;
 using StudentLoanCalculator.Api.Identity;
 using StudentLoanCalculator.Api.Models;
 using StudentLoanCalculator.Api.Data;
+using MongoDB.Driver;
 
 namespace StudentLoanCalculator.Api
 {
@@ -12,26 +13,29 @@ namespace StudentLoanCalculator.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("LocalDevelopment", builder =>
-                {
-                    builder.WithOrigins("http://localhost:4200")
-                    .WithMethods("GET", "POST");
-                });
-            });
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddSingleton<ILoanCalculator, LoanCalculator>();
 
-            // Inject database
-            //MongoCRUD db = new MongoCRUD("StudentLoanCalculatorDb");
+            // MongoDB
+            //MongoCRUD db = new MongoCRUD("StudentLoadDb");
+            //MongoCRUD db = new MongoCRUD("StudentLoadDb", builder.Configuration.GetConnectionString("StudentLoadDb"));
+            //db.InsertDefaultGrowthRates();
+            //db.InsertDefaultUser();
             //builder.Services.AddSingleton(db);
 
             var app = builder.Build();
+
+            // Cors
+            app.UseCors(IApplicationBuilder =>
+            {
+                IApplicationBuilder.WithOrigins("http://localhost:4200")
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -42,41 +46,11 @@ namespace StudentLoanCalculator.Api
 
             app.UseHttpsRedirection();
 
-            app.UseCors("LocalDevelopment");
-
             app.UseAuthorization();
 
             app.MapControllers();
 
             app.Run();
-
-        }
-
-        private void InsertDefaultUser()
-        {
-            MongoCRUD db = new MongoCRUD("StudentLoanCalculatorDb");
-
-            UserModel defaultUser = new UserModel();
-            defaultUser.FirstName = "Jonathan";
-            defaultUser.LastName = "Mantello";
-            defaultUser.EmailAddress = "jmantello@emailadress.com";
-            defaultUser.SavedCalculations = new List<SavedCalculationModel>();
-
-            InputModel inputModel = new InputModel();
-            inputModel.DiscretionaryIncome = 1500;
-            inputModel.LoanAmount = 40000;
-            inputModel.InterestRate = 5.8;
-            inputModel.TermInYears = 20;
-            inputModel.MinimumPayment = 200;
-            inputModel.InvestmentGrowthRate = 10.5;
-
-            SavedCalculationModel savedCalculation = new SavedCalculationModel();
-            savedCalculation.Name = "My saved calculation";
-            savedCalculation.InputModel = inputModel;
-
-            defaultUser.SavedCalculations.Add(savedCalculation);
-
-            db.InsertRecord("Users", defaultUser);
         }
     }
 }
